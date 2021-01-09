8ba0cfddab4b128dad291ca5626100aad054794f
purnima <pooyax14@gmail.com>
2021-01-09 14:52:29 +0530

    add .env file

--- src/config/web-sockets.js
+++ src/config/web-sockets.js
@@ -1,4 +1,10 @@
 import io from 'socket.io-client';
 
-const STRAPI_ENDPOINT = 'http://localhost:1337';
+let STRAPI_ENDPOINT;
+if (process.env.NODE_ENV !== 'production') {
+    STRAPI_ENDPOINT = 'http://localhost:1337';
+} else {
+    STRAPI_ENDPOINT = process.env.REACT_APP_SERVER_URL
+}
+
 export const socket = io(STRAPI_ENDPOINT);
